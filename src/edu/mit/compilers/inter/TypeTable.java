f080eaaa0b537320f5c9f0b8a63751e13dc326b6
hanryhu <h.henryhu@gmail.com>
2019-09-30 15:38:18 -0400

    Allow TypeTable get/set(String type)

--- src/edu/mit/compilers/inter/TypeTable.java
+++ src/edu/mit/compilers/inter/TypeTable.java
@@ -14,4 +14,41 @@ public class TypeTable extends HashMap<Integer, TypeDescriptor> {
         this.put(Type.STRING, new TypeDescriptor()); // string
         this.put(Type.STRING + Type.ARRAY, new TypeDescriptor()); // string[]
     }
+
+    private static int switchType(String type) {
+        int typeInt;
+        switch (type) {
+            case "int":
+                typeInt = Type.INT;
+                break;
+            case "int[]":
+                typeInt = Type.INT + Type.ARRAY;
+                break;
+            case "bool":
+                typeInt = Type.BOOL;
+                break;
+            case "bool[]":
+                typeInt = Type.BOOL + Type.ARRAY;
+                break;
+            case "string":
+                typeInt = Type.STRING;
+                break;
+            case "string[]":
+                typeInt = Type.STRING + Type.ARRAY;
+                break;
+            default:
+                throw new UnsupportedOperationException("Unsupported type: " + type);
+        }
+        return typeInt;
+    }
+
+    public TypeDescriptor get(String type) {
+        int typeInt = switchType(type);
+        return this.get(typeInt);
+    }
+
+    public TypeDescriptor put(String type, TypeDescriptor descriptor) {
+        int typeInt = switchType(type);
+        return this.put(typeInt, descriptor);
+    }
 }
