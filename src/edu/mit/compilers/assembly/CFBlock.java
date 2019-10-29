68e4d526abdec04faaa5b338c72c50cd2fc41a71
hanryhu <h.henryhu@gmail.com>
2019-10-29 14:39:35 -0400

    Remove Main import

--- src/edu/mit/compilers/assembly/CFBlock.java
+++ src/edu/mit/compilers/assembly/CFBlock.java
@@ -7,8 +7,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import edu.mit.compilers.Main;
-
 import edu.mit.compilers.inter.ImportTable;
 import edu.mit.compilers.inter.VariableTable;
 import edu.mit.compilers.parser.Statement;
