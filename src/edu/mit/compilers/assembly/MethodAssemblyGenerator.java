343bbc819398c19cb8468921014160c558894a8e
Jordan Docter <jdocter@mit.edu>
2019-12-10 02:19:20 -0500

    16 byte

--- src/edu/mit/compilers/assembly/MethodAssemblyGenerator.java
+++ src/edu/mit/compilers/assembly/MethodAssemblyGenerator.java
@@ -325,8 +325,7 @@ public class MethodAssemblyGenerator implements CFVisitor, MiniCFVisitor, Statem
         // this has to be done before pushing args onto stack
         // maintain 16-byte alignment -- assuming 16 aligned before call
         if (stackArgs > 0 && (stackArgs+regsToSave.size()) % 2 != 0 || regsToSave.size()%2 != 0) {
-            stackArgs++;
-            body.add("popq %rax # dummy argument used to maintain 16-byte alignment");
+            body.add("addq $8, %rsp # dummy argument used to maintain 16-byte alignment");
         }
 
         body.add("cmpq $0, " + AssemblyFactory.GLOBAL_EXIT_CODE);
